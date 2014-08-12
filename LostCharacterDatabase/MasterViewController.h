ea4a01a489b1dd208b1b1339bfb9c6b07a83f4c5
Efrén Reyes <efren.reyes@globant.com>
2014-08-12 13:35:02 -0500

    Initial Commit

---
+++ LostCharacterDatabase/MasterViewController.h
@@ -0,0 +1,18 @@
+//
+//  MasterViewController.h
+//  LostCharacterDatabase
+//
+//  Created by Efrén Reyes Torres on 8/12/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import <CoreData/CoreData.h>
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+@end
